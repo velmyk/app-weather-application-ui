d7372f561598a4ff88ac07ebed2cc14cf9d25606
SergiiKudriashov <Sergii_Kudriashov@epam.com>
2015-08-25 03:00:11 +0300

    [ENV] Added packeges. Added gulp, bootstrap, angular ...

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+concat = require('gulp-concat-css'),
+minify = require('gulp-minify-css'),
+rename = require('gulp-rename'),
+sass = require('gulp-sass'),
+jshint = require('gulp-jshint'),
+
+gulp.task('default', function() {
+});
+
