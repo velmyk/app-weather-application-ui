622e0c020502dbeffc803ef31d79fc7bf7cefec2
velmyk <velmyk@gmail.com>
2015-08-27 06:37:57 +0300

    [ENV] Change task name to jshint. Add pathes declaration

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,53 @@ var gulp = require('gulp'),
     useref = require('gulp-useref'),
     jshint = require('gulp-jshint');
 
+var path = {
+      build: { //Тут мы укажем куда складывать готовые после сборки файлы
+          html: 'build/',
+          js: 'build/js/',
+          css: 'build/css/'
+      },
+      src: { //Пути откуда брать исходники
+          html: 'src/index.html', 
+          js: 'src/app/**/*.js',
+          style: 'src/assets/scss/**/*.scss'
+      },
+      watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+          html: 'src/index.html',
+          js: 'src/app/**/*.js',
+          style: 'src/assets/scss/**/*.scss'
+      },
+      clean: './build'
+};
+
 gulp.task("build:styles", function () {
-    gulp.src("src/assets/scss/**/*.scss")
+    gulp.src(path.src.style)
         .pipe(sass())
         .pipe(concat("all.min.css"))
         .pipe(cssmin())
-        .pipe(gulp.dest("build/css/")) 
+        .pipe(gulp.dest(path.build.css)) 
         .pipe(connect.reload());
 });
 
 gulp.task("build:app", function () {
     var assets = useref.assets();
 
-    return gulp.src("src/index.html")
+    return gulp.src(path.src.html)
                .pipe(assets)
                .pipe(gulpif('*.js', uglify()))
                .pipe(assets.restore())
                .pipe(useref())
-               .pipe(gulp.dest('build'))
+               .pipe(gulp.dest(path.build.html))
                .pipe(connect.reload());
 });
 
-gulp.task('lint', function() {
-  return gulp.src('src/app/**/*.js')
+gulp.task('jshint', function() {
+  return gulp.src(path.src.js)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task("server:run", ["build:styles","build:app","watch", "lint"], function () {
+gulp.task("server:run", ["build:styles","build:app","watch", "jshint"], function () {
 
     connect.server({
         root: "build",
@@ -44,9 +63,9 @@ gulp.task("server:run", ["build:styles","build:app","watch", "lint"], function (
 })
 
 gulp.task("watch", function () {
-    gulp.watch("src/index.html", ["build:app"]);
-    gulp.watch("src/assets/scss/**/*.scss",["build:styles","build:app"]);
-    gulp.watch("src/app/**/*.js",["lint", "build:app"]);
+    gulp.watch(path.src.html, ["build:app"]);
+    gulp.watch([path.watch.style],["build:styles","build:app"]);
+    gulp.watch(path.watch.js,["jshint", "build:app"]);
 });
 
 gulp.task("default", ["server:run"]);
