9c01d7d156103b56b577371f8970df1109db95ba
Andrew <Andriy.toporov@gmail.com>
2015-09-03 21:57:47 +0300

    Settings page. Added settings page with styles. Routing was moved from app.module.js to app.routes.js

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,24 +4,5 @@
 		.module('app',[
 			'templates',
 			'ui.router'
-		])
-		.config( 
-			function($stateProvider, $urlRouterProvider) {
-
-				$urlRouterProvider.otherwise('/home');
-
-				$stateProvider
-					.state("home",{
-						url: "/home",
-						templateUrl: "components/home/homeView.html",
-						controller: "homeController"
-
-					})
-					.state("settings",{
-						url: "/settings",
-						templateUrl: "components/settings/settingsView.html",
-						controller: "settingsController"
-					})
-					;
-		});
+		]);
 })();
