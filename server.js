f6dd07b27e04a34dabeec0cd19d216aec062c81f
alexZogich <alex.zogich@yandex.ru>
2015-09-09 10:07:04 +0300

    configured mocks on server

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-	// server.js
+	var express    = require('express'),
+		app        = express(),
+		bodyParser = require('body-parser'),
+		weather = require("./data/weather.json"),
+		cities = require("./data/cities.json");
 
-	// BASE SETUP
-	// =============================================================================
-
-	// call the packages we need
-	var express    = require('express');        // call express
-	var app        = express();                 // define our app using express
-	var bodyParser = require('body-parser');
-
-	// configure app to use bodyParser()
-	// this will let us get the data from a POST
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
 
-	var port = process.env.PORT || 8080;        // set our port
+	var port = 8000;
+	var router = express.Router();
 
-	// ROUTES FOR OUR API
-	// =============================================================================
-	var router = express.Router();              // get an instance of the express Router
-
-	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-	router.get('/', function(req, res) {
-	    res.json({ message: 'hooray! welcome to our api!' });   
+	router.use(function(req, res, next) {
+    	next(); 
 	});
 
-	// more routes for our API will happen here
-
-	// REGISTER OUR ROUTES -------------------------------
-	// all of our routes will be prefixed with /api
 	app.use('/api', router);
 
-	// START THE SERVER
-	// =============================================================================
+	router.route('/weather/:cityId')
+		.get(function (req, res){
+			weather.forEach( function (item, i ) {
+				if (item.city.id == req.params.cityId) {
+					res.json(item);
+				} else {
+					res.json({"status": "err"});
+				}
+			});
+		});
+
+	router.route('/city/all')
+		.get(function (req, res){
+			res.json(cities);	
+		});
+	
 	app.listen(port);
-	console.log('Magic happens on port ' + port);
+	console.log('Server starts at ' + port);
