477f00f07bb962a7588c8758d77d3ae2d36c3608
alexZogich <alex.zogich@yandex.ru>
2015-09-14 14:23:56 +0300

    fixed bug

--- src/app/components/OpenWeatherAPI/OpenWeatherAPI.js
+++ src/app/components/OpenWeatherAPI/OpenWeatherAPI.js
@@ -16,17 +16,18 @@
 			city = response.data.city.name;
 
 			response.data.list.forEach( function (item, i) {
-			temperatureForecast[item.dt] = item.main.temp;
-			humidityForecast[item.dt] = item.main.humidity;
-			windForecast[item.dt] = {
-				speed: item.wind.speed,
-				degree: item.wind.deg,
-			};
-			weatherStateForecast[item.dt] = {
-				id: item.weather[0].id,
-				desc: item.weather[0].description,
-				state: item.weather[0].main
-			};
+				temperatureForecast[item.dt] = item.main.temp;
+				humidityForecast[item.dt] = item.main.humidity;
+				windForecast[item.dt] = {
+					speed: item.wind.speed,
+					degree: item.wind.deg,
+				};
+				weatherStateForecast[item.dt] = {
+					id: item.weather[0].id,
+					desc: item.weather[0].description,
+					state: item.weather[0].main
+				};
+			});
 		}
 
 		function loadForecast() {
