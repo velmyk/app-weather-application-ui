6c069f408e806798fa6895cd73bb8382f0bdaa72
velmyk <velmyk@gmail.com>
2015-09-28 23:02:11 +0300

    Add unit-tests for temperature directive

---
+++ src/app/shared/temperature/temperatureDirective.spec.js
@@ -0,0 +1,26 @@
+"use strict";
+
+describe('Custome temperature directive', function() {
+
+    var mockScope,
+        element, 
+        compiledDirective, 
+        isolateScope;
+
+    beforeEach(module("app"));
+    beforeEach(module("templates"));
+  
+	beforeEach(inject(function(_$rootScope_, _$compile_){
+    	mockScope = _$rootScope_.$new();
+    	element = angular.element('<temperature data-time="1449882061"></temperature>');
+    	compiledDirective = _$compile_(element)(mockScope);
+    	mockScope.$digest();
+    	isolateScope = element.isolateScope();
+
+  	}));
+
+  	it('has valid data in isolate scope', function() {
+  		expect(isolateScope.time).toEqual("1449882061");
+  	});
+
+});
