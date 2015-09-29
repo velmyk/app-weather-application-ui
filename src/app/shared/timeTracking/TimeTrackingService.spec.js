7aac5caa4efaeec48908fd3c97576c6e30cee0f5
velmyk <velmyk@gmail.com>
2015-09-29 11:27:10 +0300

    Fixed unit-test for TimeTrackingService

--- src/app/shared/timeTracking/TimeTrackingService.spec.js
+++ src/app/shared/timeTracking/TimeTrackingService.spec.js
@@ -4,7 +4,8 @@ describe('TimeTrackingService', function() {
     var sut,
     	time,
     	maxDay = 4,
-        MOCK_TIME_IN_SECONDS;
+        MOCK_TIME_IN_SECONDS,
+        mock_localStorageService;
 
     beforeEach(module('app'));
 
@@ -13,18 +14,33 @@ describe('TimeTrackingService', function() {
             $provide.service('TIME_IN_SECONDS', function() {
                 this.DAY = 86400;
             });
+
+            $provide.service('localStorageService', function() {
+                this.get = jasmine.createSpy('get').and.returnValue("5");
+            });
         });
     });
 
-    beforeEach(inject(function(_$rootScope_, _TimeTrackingService_, TIME_IN_SECONDS) {
+    beforeEach(inject(function(_$rootScope_, _TimeTrackingService_, TIME_IN_SECONDS, localStorageService) {
         MOCK_TIME_IN_SECONDS = TIME_IN_SECONDS;
         sut = _TimeTrackingService_;
+        mock_localStorageService = localStorageService;
         sut.initTime();
     }));
 
     function calculateTime (days) {
 		return (new Date((sut.time.now + days * MOCK_TIME_IN_SECONDS.DAY)*1000).setHours(15))/1000;
 	}
+
+    function getMaxDays() {
+        return localStorageService.get("daysToDisplay") || 3;
+    }
+
+    it("should get data from localStorage", function() {
+        var maxDays = mock_localStorageService.get();
+
+        expect(sut.time.maxDays).toEqual(maxDays);
+    });
 
     it("should plus day to init data", function() {
     	var displayDayBefore = sut.time.displayDay;
@@ -34,7 +50,7 @@ describe('TimeTrackingService', function() {
 
         expect(sut.time.displayTime).toEqual(calculateTime(displayDayBefore));
         expect(sut.time.displayDay).toEqual(displayDayBefore);
-    })
+    });
 
     it("should not plus day if max days reached", function() {
     	sut.plusDay();
