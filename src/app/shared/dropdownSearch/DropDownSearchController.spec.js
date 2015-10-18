571978a61efa202667272367e9a656df2b4ec3e6
velmyk <velmyk@gmail.com>
2015-10-18 19:36:17 +0300

    Performed mocking of City Service with

--- src/app/shared/dropdownSearch/DropDownSearchController.spec.js
+++ src/app/shared/dropdownSearch/DropDownSearchController.spec.js
@@ -6,31 +6,36 @@ describe('Dropdown Serch Controller', function(){
 			CityService,
 			localStorageService,
 			sut,
-			PreloaderService;
+			mockCityService,
+			$rootScope,
+			$controller,
+			$q;
 
 	beforeEach(module('app'));
 
 	beforeEach(module(function($provide){
 
-		$provide.value('CityService', {
-			getCities: function(searchPhrase) {
-				return {
-					then: function(callback) {
-						return callback( { data: { cnt: 2, list: [ { _id: 123456, name: ['Kiev'] }, { _id: 654321, name: ['London'] } ] } } );
-					}
-				};
+		mockCityService = {
+			getCities: function (searchPhrase) {
+				var deferred = $q.defer();
+				deferred.resolve( { data: { cnt: 2, list: [ { _id: 123456, name: ['Kiev'] }, { _id: 654321, name: ['London'] } ] } } );
+				return deferred.promise;
 			}
-		});
+		}
+
+		$provide.value('CityService', mockCityService);
 
 	}));
 
-	beforeEach(inject(function(_$rootScope_, _$controller_, _CityService_, _localStorageService_, _PreloaderService_){
-		mockScope = _$rootScope_.$new();
-		sut = _$controller_('DropdownSearchController', {$scope: mockScope});
-		mockScope.vm = sut;
+	beforeEach(inject(function(_$rootScope_, _$controller_, _CityService_, _localStorageService_, _$q_){
+		$rootScope = _$rootScope_;
+		$q = _$q_;
+		$controller = _$controller_;
 		CityService = _CityService_;
 		localStorageService = _localStorageService_;
-		PreloaderService = _PreloaderService_;
+		mockScope = $rootScope.$new();
+		sut = $controller('DropdownSearchController', {$scope: mockScope});
+		mockScope.vm = sut;
 	}));
 
 	it('should have empty object city when settings page opens', function(){
@@ -51,6 +56,8 @@ describe('Dropdown Serch Controller', function(){
 
 	it('should get array from server in respond', function(){
 		sut.refreshCities('abcd');
+		$rootScope.$apply();
+		console.log(sut.cities);
 		expect(sut.cities).toEqual(jasmine.any(Array));
 		expect(sut.cities.length).toEqual(2);
 	});
